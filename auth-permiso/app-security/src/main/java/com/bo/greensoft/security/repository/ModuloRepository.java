92be98a06bd5f8f59bf445ff7916579e24f5734d
vmrimberth <rimberth.vm@gmail.com>
2024-05-12 02:19:36 -0400

    Adicion de variables de entorono y volumen en docker compose

--- auth-permiso/app-security/src/main/java/com/bo/greensoft/security/repository/ModuloRepository.java
+++ auth-permiso/app-security/src/main/java/com/bo/greensoft/security/repository/ModuloRepository.java
@@ -9,7 +9,7 @@ import com.bo.greensoft.security.model.Modulo;
 @Repository
 public interface ModuloRepository extends JpaRepository<Modulo, Integer>{
 
-	@Query(value = "SELECT MAX(m.id) FROM modulo m", nativeQuery = true, countQuery = "select count(*) from public.modulo")
+	@Query(value = "SELECT MAX(m.id) FROM public.modulo m", nativeQuery = true, countQuery = "select count(*) from public.modulo")
 	public Integer findMaxMid();
 	
 }
--- auth-permiso/app-security/src/main/java/com/bo/greensoft/security/repository/UsuarioRepository.java
+++ auth-permiso/app-security/src/main/java/com/bo/greensoft/security/repository/UsuarioRepository.java
@@ -10,6 +10,6 @@ import com.bo.greensoft.security.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
-	@Query(value = "SELECT MAX(u.id) FROM permiso u", nativeQuery = true, countQuery = "select count(*) from public.usuario")
+	@Query(value = "SELECT MAX(u.id) FROM public.usuario u", nativeQuery = true, countQuery = "select count(*) from public.usuario")
 	public Integer findMaxUid();
 }
